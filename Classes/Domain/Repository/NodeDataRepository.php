913f9d50b1df7735c45bb35cfff080fc346166fd
Jonas Reuter <jonas.reuter@medienreaktor.de>
2025-08-11 10:43:58 +0200

    [TASKS] Bugfix

--- Classes/Domain/Repository/NodeDataRepository.php
+++ Classes/Domain/Repository/NodeDataRepository.php
@@ -49,7 +49,7 @@ class NodeDataRepository extends Repository
         string $dimensionsHash = null
     ): IterableResult {
         $queryBuilder = $this->entityManager->createQueryBuilder();
-        $queryBuilder->select('n.Persistence_Object_Identifier persistenceObjectIdentifier, n.identifier identifier, n.dimensionsHash dimensions, n.nodeType nodeType, n.path path')
+        $queryBuilder->select('n.Persistence_Object_Identifier persistenceObjectIdentifier, n.identifier identifier, n.dimensionValues dimensions, n.nodeType nodeType, n.path path')
             ->from(NodeData::class, 'n')
             ->where('n.workspace = :workspace AND n.removed = :removed AND n.movedTo IS NULL')
             ->setMaxResults((integer)$maxResults)
